aa124cb2e0a9342cfb720fcb69486c0bb8daa679
Hector Herrera <herrera.hector1998@gmail.com>
2020-06-06 11:56:03 -0500

    arrow operator in struct

--- island.c
+++ island.c
@@ -11,6 +11,16 @@ const int island[5][7] = {{0, 0, 0, 0, 0, 0, 0},
 
 int positions[5][7] = {0};
 
+pthread_mutex_t lockTurn;
+
+struct ballData {
+    pthread_t id;
+    int x;
+    int y;
+};
+
+struct ballData* balls;
+
 void printArray(int array[][7]){
     int i, j;
     for(i = 0; i < 5;i++){
@@ -26,9 +36,19 @@ void *ballBehaviour(void *threadId)
     long tid = (long)threadId;
     int speed = 1000; // 1 second
     int finished = 1;
-    int x = rand() % 5;
-    int y = rand() % 7;
-    printf("init pos: [%d][%d]\n", x, y);
+    while(1){
+        int x = rand() % 5;
+        int y = rand() % 7;
+        printf("init pos: [%d][%d]\n", x, y);
+
+        pthread_mutex_lock(&lockTurn);
+        if(positions[x][y] == 0){
+            positions[x][y] = 1;
+        }
+        pthread_mutex_unlock(&lockTurn);
+    }
+    
+
     while(finished){
         usleep(speed * 1000); // sleep in microseconds
         printf("Printing GeeksQuiz from Thread %ld \n", tid);
@@ -46,25 +66,28 @@ int main(int argc, char** argv){
         return -1;
     }
 
-    int balls = atoi(argv[1]);
-    if(balls <= 0){
+    int n = atoi(argv[1]);
+    if(n <= 0){
         printf("Error, number of balls must a number or be higher than 0\n");
         return -1;
     }
 
-    pthread_t threads[balls];
+    // pthread_t threads[n];
+    balls = malloc(sizeof(struct ballData)*n);
     int rc;
     long t;
-    for(t=0; t<balls; t++){
+    pthread_t id;
+    for(t=0; t<n; t++){
         // printf("In main: creating thread %ld\n", t);
-        rc = pthread_create(&threads[t], NULL, ballBehaviour, (void *)t);
+        id = balls[t]->id;
+        rc = pthread_create(&id, NULL, ballBehaviour, (void *)t);
         if (rc){
             printf("ERROR; return code from pthread_create() is %d\n", rc);
             exit(-1);
         }
     }
-    for(t=0;t<balls;t++){
-        pthread_join(threads[t], NULL);
+    for(t=0;t<n;t++){
+        pthread_join(balls[t]->id, NULL);
     }
     
     printf("Main for Island.c\n");
